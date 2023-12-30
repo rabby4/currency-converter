15817c253aada52a39e2c73e761100f0f5dc8557
rabby4 <mdrabbybd4747@gmail.com>
2023-12-30 21:38:07 +0600

    added some customization for converter function

--- src/hooks/useCurrencyInfo.jsx
+++ src/hooks/useCurrencyInfo.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-const useCurrencyInfo = ({ currency }) => {
+const useCurrencyInfo = (currency) => {
     const [data, setData] = useState({})
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
             .then(res => res.json())
-            .then(data => setData(data))
+            .then(res => setData(res[currency]))
     }, [currency])
     console.log(data)
     return data
