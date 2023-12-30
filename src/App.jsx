192bdb7bc04c22f68ef6d72f6f3c51dda16bdf0a
rabby4 <mdrabbybd4747@gmail.com>
2023-12-30 21:20:39 +0600

    create swap box

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 
+import { useState } from 'react'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('bdt')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+
+  }
 
   return (
     <>
